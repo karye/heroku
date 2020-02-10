fd0d058849d0dd834e181a3d706e42c9e102c932
karye <karye.webb@gmail.com>
2020-02-10 13:07:32 +0100

    Bara PHP och CSS

--- web/index.php
+++ web/index.php
@@ -4,11 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="./stylesheets/style.css">
 </head>
 <body>
-    <?php
-    echo "<p>Testar Heroku!</p>";
-    ?>
+    <div class="container">
+        <h1>Testar Heroku</h1>
+        <?php
+        echo "<p>Testar Heroku!</p>";
+        ?>
+    </div>
 </body>
 </html>
