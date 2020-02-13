5e1aa032fc4bc7e147cf2e5618b19f43022ee10b
karye <karye.webb@gmail.com>
2020-02-13 12:36:10 +0100

    Testar spara och läsa cookies

--- web/test.php
+++ web/test.php
@@ -34,11 +34,11 @@ setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1
         </nav>
         <main>
         <?php
-            if (!isset($_COOKIE[$cookie_name])) {
-                echo "Cookie named '" . $cookie_name . "' is not set!";
+            if (!isset($_COOKIE["user"])) {
+                echo "Cookie named 'user' is not set!";
             } else {
-                echo "Cookie '" . $cookie_name . "' is set!<br>";
-                echo "Value is: " . $_COOKIE[$cookie_name];
+                echo "Cookie 'user' is set!<br>";
+                echo "Value is: " . $_COOKIE[$cookie_value];
             }
             ?>
         </main>
