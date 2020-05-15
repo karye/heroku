22e5a6a971d0f37bcab378ee32e30b0c84d2e76b
karye <karye.webb@gmail.com>
2020-05-15 08:53:45 +0200

    typo

--- web/meny-include.php
+++ web/meny-include.php
@@ -1,9 +1,11 @@
 <?php
 $sida = basename($_SERVER['PHP_SELF'], ".php");
+var_dump($sida);
 ?>
 <ul class="nav nav-tabs">
-    <li class="nav-item"><a class="nav-link <?= ($activePage == 'index') ? 'active':''; ?>" href="./index.php">Läsa</a></li>
-    <li class="nav-item"><a class="nav-link <?= ($activePage == 'index') ? 'active':''; ?>" href="./insert.php">Skriva</a></li>
-    <li class="nav-item"><a class="nav-link <?= ($activePage == 'index') ? 'active':''; ?>" href="./table.php">Skapa tabell</a></li>
-    <li class="nav-item"><a class="nav-link <?= ($activePage == 'index') ? 'active':''; ?>" href="./test.php">Test</a></li>
+    <li class="nav-item"><a class="nav-link <?= ($sida == 'index') ? 'active':''; ?>" href="./index.php">Läsa</a></li>
+    <li class="nav-item"><a class="nav-link <?= ($sida == 'insert') ? 'active':''; ?>" href="./insert.php">Skriva</a></li>
+    <li class="nav-item"><a class="nav-link <?= ($sida == 'table') ? 'active':''; ?>" href="./table.php">Skapa tabell</a></li>
+    <li class="nav-item"><a class="nav-link <?= ($sida == 'fill') ? 'active':''; ?>" href="./table.php">Fylla tabell</a></li>
+    <li class="nav-item"><a class="nav-link <?= ($sida == 'test') ? 'active':''; ?>" href="./test.php">Test</a></li>
 </ul>
