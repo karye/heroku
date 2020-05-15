21a150fb8a00fc6377ec4f00cdc6c226d53dd40b
karye <karye.webb@gmail.com>
2020-05-15 09:30:08 +0200

    Fix

--- web/meny-include.php
+++ web/meny-include.php
@@ -5,7 +5,7 @@ $sida = basename($_SERVER['PHP_SELF'], ".php");
     <li class="nav-item"><a class="nav-link <?= ($sida == 'index') ? 'active' : ''; ?>" href="./index.php">Läsa</a></li>
     <li class="nav-item"><a class="nav-link <?= ($sida == 'insert') ? 'active' : ''; ?>" href="./insert.php">Skriva</a></li>
     <li class="nav-item"><a class="nav-link <?= ($sida == 'table') ? 'active' : ''; ?>" href="./table.php">Skapa tabell</a></li>
-    <li class="nav-item"><a class="nav-link <?= ($sida == 'fill') ? 'active' : ''; ?>" href="./table.php">Fylla tabell</a></li>
-    <li class="nav-item"><a class="nav-link <?= ($sida == 'drop') ? 'active' : ''; ?>" href="./table.php">Radera tabell</a></li>
+    <li class="nav-item"><a class="nav-link <?= ($sida == 'fill') ? 'active' : ''; ?>" href="./fill.php">Fylla tabell</a></li>
+    <li class="nav-item"><a class="nav-link <?= ($sida == 'drop') ? 'active' : ''; ?>" href="./drop.php">Radera tabell</a></li>
     <li class="nav-item"><a class="nav-link <?= ($sida == 'test') ? 'active' : ''; ?>" href="./test.php">Test</a></li>
 </ul>
